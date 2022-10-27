beadca2b7a577724eb6089a4aa391946ed0f809a
SweetyBug <maxim_terekhin@bk.ru>
2022-10-27 23:19:56 +0300

    Finish code in new branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
 void main() {
-  print('It\'s work!');
+  var dir = Directory('C:/Users/11/Desktop/Projects/Flutter/files');
+  List<FileSystemEntity> files = dir.listSync().toList();
+  for (var file in files) {
+    print(file);
+  }
 }
