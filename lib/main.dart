029f9eeec8c6ad9c16752ca849fefb3123ff9f63
SweetyBug <maxim_terekhin@bk.ru>
2022-10-27 23:29:34 +0300

    Change for to forEach

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,5 @@ import 'dart:io';
 void main() {
   var dir = Directory('C:/Users/11/Desktop/Projects/Flutter/files');
   List<FileSystemEntity> files = dir.listSync().toList();
-  for (var file in files) {
-    print(file);
-  }
+  files.forEach((print));
 }
